9dfae643b4dabd9c18d3a504f61aaa0c3184446d
Pawel Biesiada <pawel.piotr.biesiada@gmail.com>
2025-06-08 23:46:19 +0200

    fixed issue with depreciated nugets

--- DIProject/Model/User.cs
+++ DIProject/Model/User.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace DIProject.Model
 {
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Password { get; set; }
+        public string Name { get; set; } = null!;
+        public string Password { get; set; } = null!;
         public bool IsActive { get; set; }
     }
 }
